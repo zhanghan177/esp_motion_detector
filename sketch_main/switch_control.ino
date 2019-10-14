1997ac71b3df9d25ad6e7e55783c8f3388edeeb7
Han Zhang <han@zhanghan177.me>
2019-10-13 22:35:01 -0400

    Add methods to use IFTTT to control smart plug

--- sketch_main/switch_control.ino
+++ sketch_main/switch_control.ino
@@ -1,3 +1,68 @@
-void switch_turn_on() {}
+#include <HTTPClient.h>
+#include "IFTTT_config.h"
 
-void switch_turn_off() {}
+const int MAX_BUFFER_LEN = 1024;
+const char* root_ca = \
+                      "-----BEGIN CERTIFICATE-----\n" \
+                      "MIIDxTCCAq2gAwIBAgIBADANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx\n" \
+                      "EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT\n" \
+                      "EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp\n" \
+                      "ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTA5MDkwMTAwMDAwMFoXDTM3MTIzMTIz\n" \
+                      "NTk1OVowgYMxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH\n" \
+                      "EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjExMC8GA1UE\n" \
+                      "AxMoR28gRGFkZHkgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkgLSBHMjCCASIw\n" \
+                      "DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAL9xYgjx+lk09xvJGKP3gElY6SKD\n" \
+                      "E6bFIEMBO4Tx5oVJnyfq9oQbTqC023CYxzIBsQU+B07u9PpPL1kwIuerGVZr4oAH\n" \
+                      "/PMWdYA5UXvl+TW2dE6pjYIT5LY/qQOD+qK+ihVqf94Lw7YZFAXK6sOoBJQ7Rnwy\n" \
+                      "DfMAZiLIjWltNowRGLfTshxgtDj6AozO091GB94KPutdfMh8+7ArU6SSYmlRJQVh\n" \
+                      "GkSBjCypQ5Yj36w6gZoOKcUcqeldHraenjAKOc7xiID7S13MMuyFYkMlNAJWJwGR\n" \
+                      "tDtwKj9useiciAF9n9T521NtYJ2/LOdYq7hfRvzOxBsDPAnrSTFcaUaz4EcCAwEA\n" \
+                      "AaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYwHQYDVR0OBBYE\n" \
+                      "FDqahQcQZyi27/a9BUFuIMGU2g/eMA0GCSqGSIb3DQEBCwUAA4IBAQCZ21151fmX\n" \
+                      "WWcDYfF+OwYxdS2hII5PZYe096acvNjpL9DbWu7PdIxztDhC2gV7+AJ1uP2lsdeu\n" \
+                      "9tfeE8tTEH6KRtGX+rcuKxGrkLAngPnon1rpN5+r5N9ss4UXnT3ZJE95kTXWXwTr\n" \
+                      "gIOrmgIttRD02JDHBHNA7XIloKmf7J6raBKZV8aPEjoJpL1E/QYVN8Gb5DKj7Tjo\n" \
+                      "2GTzLH4U/ALqn83/B2gX2yKQOC16jdFU8WnjXzPKej17CuPKf1855eJ1usV2GDPO\n" \
+                      "LPAvTK33sefOT6jEm0pUBsV/fdUID+Ic/n4XuKxe9tQWskMJDE32p2u0mYRlynqI\n" \
+                      "4uJEvlz36hz1\n" \
+                      "-----END CERTIFICATE-----\n" ;
+
+
+char *ifttt_webhook = "https://maker.ifttt.com/trigger/%s/with/key/%s";
+
+void switch_turn_on() {
+  char *cmd = "turn_on_bedroom";
+
+  send_to_ifttt(cmd);
+}
+
+void switch_turn_off() {
+  char *cmd = "turn_off_bedroom";
+
+  send_to_ifttt(cmd);
+}
+
+void send_to_ifttt(char *cmd) {
+  char full_url[MAX_BUFFER_LEN];
+  memset(full_url, 0, MAX_BUFFER_LEN);
+  snprintf(full_url, MAX_BUFFER_LEN, ifttt_webhook, cmd, ifttt_key);
+
+  Serial.println(full_url);
+
+  HTTPClient http;
+  http.begin(full_url, root_ca); //Specify destination for HTTP request
+  int httpCode = http.POST(""); //Make the request
+
+  if (httpCode > 0) { //Check for the returning code
+
+    String payload = http.getString();
+    Serial.println(httpCode);
+    Serial.println(payload);
+  }
+
+  else {
+    Serial.println("Error on HTTP request");
+  }
+
+  http.end(); //Free the resources
+}
